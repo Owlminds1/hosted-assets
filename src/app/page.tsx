425d0e7a9759ac308eb7478eef940858a97ba322
mohammad asad <asadkhan41376@gmail.com>
2025-02-01 08:56:41 +0530

    home page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
+import linkData from "@/lib/linkData.json"
 
 const page = () => {
   return (
-    <div>
+    <div className=' bg-[url("/bg/bg2.jpg")] bg-no-repeat bg-cover bg-bottom flex justify-center gap-10 items-center min-h-screen w-full flex-col'>
+      <h2 className='text-[50px] text-red-800 font-bold '>Hosted Assets</h2>
+
+      <ul className='flex flex-wrap gap-3 justify-center'>
+        {
+          linkData.map((item,index)=>(
+
+            <Link className='border px-3 rounded-lg py-1 min-w-[100px] text-center bg-red-600 hover:bg-red-700' key={index} href={`/${item.page}`}>{item.page}</Link>
+          ))
+        }
+        <li>
+
+        </li>
+      </ul>
       
     </div>
   )
