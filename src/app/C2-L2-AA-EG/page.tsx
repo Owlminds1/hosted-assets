9b416a1da05dd5c2699a85b4fbed04c8b41ad7f4
mohammad asad <asadkhan41376@gmail.com>
2025-02-08 11:49:38 +0530

    add two new activity

---
+++ src/app/C2-L2-AA-EG/page.tsx
@@ -0,0 +1,18 @@
+// C2-L2-AA-EG
+
+import React from 'react'
+
+const page = () => {
+  return (
+
+    <div className='min-h-screen bg-slate-50 flex items-center justify-center'>
+    <iframe width='640' height='480' src='//www.tinytap.com/activities/g3jpz/player/embed/'  allowFullScreen >    </iframe>
+   </div>
+
+  )
+}
+
+export default page
+
+
+
---
+++ src/app/C6-L1-PADT/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MyImage from "@/components/image";
+
+const page = () => {
+  return (
+ // C6-L1-PADT
+    <div className="min-h-screen bg-slate-50 p-5 flex items-center justify-center">
+      <MyImage src="/images/C6-L1-PADT.png" />
+    </div>
+  );
+};
+
+export default page;
