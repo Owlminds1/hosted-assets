459a1b2afd475713dcb2672b09a69ff9c4d0b0c1
mohammad asad <asadkhan41376@gmail.com>
2025-02-20 10:55:17 +0530

    new ativity C2-L2-A1

---
+++ src/app/C2-L2-A1/page.tsx
@@ -0,0 +1,17 @@
+// C2-L2-A1
+import React from 'react'
+
+const Page = () => {
+  return (
+
+    <div className='min-h-screen bg-slate-50 flex items-center justify-center'>
+    <iframe width='640' height='480' src='//www.tinytap.com/activities/g3khc/player/embed/'  allowFullScreen >    </iframe>
+   </div>
+
+  )
+}
+
+export default Page
+
+
+
