c71fe363346788cd9fdbee5fbecbeb352faa9549
mohammad asad <asadkhan41376@gmail.com>
2025-02-22 15:14:09 +0530

    change in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,35 @@ import linkData from "@/lib/linkData.json"
 
 const page = () => {
   return (
-    <div className=' bg-[url("/bg/bg2.jpg")] bg-no-repeat bg-cover bg-bottom flex justify-center gap-10 items-center min-h-screen w-full flex-col'>
+    <div className="min-h-screen h-screen overflow-y-scroll flex flex-col gap-4 items-center justify-start  bg-[url(/bg/bg2.jpg)] bg-cover bg-bottom p-5 w-full">
       <h2 className='text-[50px] text-red-800 font-bold '>Hosted Assets</h2>
+      <div className="grid grid-cols-12 gap-4 min-w-[500px] place-items-center text-center">
+        <div className="col-span-3 min-w-[100px] font-bold border border-black bg-purple-500 py-1 px-4 rounded-lg">
+          Sr. no.
+        </div>
+        <div className="col-span-6 min-w-[500px] font-bold border border-black bg-purple-500 py-1 px-4 rounded-lg">
+          Activity Name
+        </div>
+        <div className="col-span-3 w-full border font-bold  border-black bg-purple-500 py-1 px-4 rounded-lg">
+          Activity Link
+        </div>
+      </div>
 
-      <ul className='flex flex-wrap gap-3 justify-center'>
-        {
-          linkData.map((item,index)=>(
-
-            <Link className='border px-3 rounded-lg py-1 min-w-[100px] text-center bg-red-600 hover:bg-red-700' key={index} href={`/${item.page}`}>{item.page}</Link>
-          ))
-        }
-        <li>
-
-        </li>
-      </ul>
+      {
+        linkData.map((item,index)=>(
+          <div key={index} className="grid grid-cols-12 gap-4 min-w-[500px] place-items-center text-center">
+          <div className="col-span-3 min-w-[100px] border border-black bg-red-400 py-1 px-4 text-black rounded-lg">
+            {index + 1}
+          </div>
+          <div  className="col-span-6 min-w-[500px] border border-black bg-red-400 py-1 px-4 rounded-lg">
+            {`Activity ${index + 1}`}
+          </div>
+          <div className="col-span-3 w-full border text-black border-black bg-red-400 py-1 px-4 rounded-lg">
+          <Link  href={`/${item.page}`}>{item.page}</Link>
+          </div>
+        </div>
+        ))
+      }
       
     </div>
   )
