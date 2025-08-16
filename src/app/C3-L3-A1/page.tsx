24a720347a022cadbfddc14936cc653106a5a8fe
mohammad asad <asadkhan41376@gmail.com>
2025-08-16 19:54:19 +0530

    add activity C3-L3-A1

---
+++ src/app/C3-L3-A1/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <Link href="https://87dcrn.csb.app/" className='text-3xl'>https://87dcrn.csb.app/</Link>
+    </div>
+  )
+}
+
+export default Page
