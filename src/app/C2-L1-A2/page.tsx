b108a3e35f3229a542607d473fd673f0429dc6d4
mohammad asad <asadkhan41376@gmail.com>
2025-09-04 12:25:43 +0530

    add new activity C2-L1-A2

---
+++ src/app/C2-L1-A2/page.tsx
@@ -0,0 +1,12 @@
+import LayoutC2L1A2 from '@/layoutC2-L1-A2'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <LayoutC2L1A2/>
+    </div>
+  )
+}
+
+export default Page
